15f419ef5bc38208c22b0fb04093687e618d9c7e
Ira Baker <paxpistor@gmail.com>
2026-01-30 10:02:22 +0100

    Updated

--- src/test/java/de/phonebook/tests/AddContactTests.java
+++ src/test/java/de/phonebook/tests/AddContactTests.java
@@ -4,19 +4,11 @@ import de.phonebook.core.TestBase;
 import de.phonebook.data.UserData;
 import de.phonebook.model.Contact;
 import de.phonebook.model.User;
+import de.phonebook.utils.MyDataProviders;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 public class AddContactTests extends TestBase {
     
@@ -33,7 +25,7 @@ public class AddContactTests extends TestBase {
         app.getUser().clickOnLoginButton();
     }
 
-    @Test(dataProvider = "addNewContactFromCsv")
+    @Test(dataProvider = "addNewContactFromCsv",dataProviderClass = MyDataProviders.class)
     public void addContactPositiveTest(Contact contact) {
         app.getContact().clickOnAddLink();
         app.getContact().fillAddContactForm(contact);
@@ -45,23 +37,4 @@ public class AddContactTests extends TestBase {
     public void postCondition() {
         app.getContact().removeContact();
     }
-
-    @DataProvider
-    public Iterator<Object[]> addNewContactFromCsv() throws IOException {
-        List<Object[]> list = new ArrayList<>();
-        BufferedReader reader = new BufferedReader
-                (new FileReader(new File("src/test/resources/contact.csv")));
-
-        String line = reader.readLine();
-
-        while (line!=null) {
-            String[] split = line.split(",");
-            list.add(new Object[]{new Contact().setName(split[0]).setLastname(split[1]).setPhone(split[2])
-                    .setEmail(split[3]).setAddress(split[4]).setDescription(split[5])});
-            line = reader.readLine();
-        }
-
-        return list.iterator();
-    }
-
 }
